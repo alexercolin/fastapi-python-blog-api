4e12d2699bb6095148371d2c58987f7c14828982
alexercolin <alex_ercolin@hotmail.com>
2021-11-02 08:44:59 -0300

    feat: add add/delete user operations from the database

--- blog/main.py
+++ blog/main.py
@@ -3,6 +3,7 @@ from . import schemas, models
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
 from pprint import pprint
+from typing import List
 
 app = FastAPI()
 
@@ -26,13 +27,13 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
-@app.get('/blog')
+@app.get('/blog',response_model=List[schemas.ShowBlog])
 def getAllBlogs(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
 
-@app.get('/blog/{id}', status_code=status.HTTP_200_OK)
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog)
 def getBlogById(id: int, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
@@ -65,5 +66,25 @@ def updateBlogById(id, request: schemas.Blog, db: Session = Depends(get_db)):
 
     blog.update({'title': request.title, 'body': request.body},
                 synchronize_session='evaluate')
+    db.commit()
+    return'done'
+
+@app.post('/user', status_code=status.HTTP_201_CREATED)
+def createUser(request: schemas.User, db: Session = Depends(get_db)):
+    new_user = models.User(name=request.name, email=request.email, password=request.password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+@app.delete('/user/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def deleteUser(id: int, response: Response, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id)
+
+    if not user.first():
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail='not found')
+
+    user.delete(synchronize_session=False)
     db.commit()
     return'done'
--- blog/schemas.py
+++ blog/schemas.py
@@ -3,3 +3,12 @@ from pydantic import BaseModel
 class Blog(BaseModel):
     title: str
     body: str
+
+class ShowBlog(Blog):
+    class Config():
+        orm_mode = True
+        
+class User(BaseModel):
+    name: str
+    email: str
+    password: str
