57f9e69aca2a69e29be65a9a46c959da8b217e31
alexercolin <alex_ercolin@hotmail.com>
2021-11-03 17:58:02 -0300

    refactor: Change table name in the model

--- blog/models.py
+++ blog/models.py
@@ -10,7 +10,7 @@ class Blog(Base):
 
 
 class User(Base):
-    __tablename__ = 'blogUsers'
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
     email = Column(String)
