f1390b8ce5ed23e413172f0be89dbec929fd87ae
alexercolin <alex_ercolin@hotmail.com>
2021-11-04 15:06:15 -0300

    refactor: Separate users and blogs routers

---
+++ blog/routers/blog.py
@@ -0,0 +1,36 @@
+from fastapi import APIRouter, Depends, status, Response, HTTPException
+from .. import schemas, database, models
+from typing import List
+from sqlalchemy.orm import Session
+from ..repository import blog
+
+
+router = APIRouter(
+    prefix="/blog",
+    tags=['Blogs']
+)
+
+
+@router.get('/', response_model=List[schemas.ShowBlog])
+def get_all(db: Session = Depends(database.get_db)):
+    return blog.get_all(db)
+
+
+@router.get('/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog)
+def get_by_id(id: int, response: Response, db: Session = Depends(database.get_db)):
+    return blog.get_by_id(id, response, db)
+
+
+@router.post('/', status_code=status.HTTP_201_CREATED)
+def create(request: schemas.Blog, db: Session = Depends(database.get_db)):
+    return blog.create(request, db)
+
+
+@router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete(id: int, response: Response, db: Session = Depends(database.get_db)):
+    return blog.delete(id, response, db)
+
+
+@router.put('/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: schemas.Blog, db: Session = Depends(database.get_db)):
+    return blog.update(id, request, db)
---
+++ blog/routers/users.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter, Depends, status, Response, HTTPException
+from .. import schemas, database, models
+from typing import List
+from sqlalchemy.orm import Session
+from ..repository import users
+
+
+router = APIRouter(
+    prefix='/user',
+    tags=['Users']
+
+)
+
+@router.get('/', response_model=List[schemas.Users])
+def get_all(db: Session = Depends(database.get_db)):
+    return users.get_all(db)
+
+@router.get('/{id}', status_code=status.HTTP_200_OK, response_model=schemas.Users)
+def get_by_id(id: int, db: Session = Depends(database.get_db)):
+    return users.get_by_id(id,response,db)
+
+
+@router.post('/', status_code=status.HTTP_201_CREATED)
+def create(request: schemas.User, db: Session = Depends(database.get_db)):
+    return users.create(request, db)
+
+
+@router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete(id: int, response: Response, db: Session = Depends(database.get_db)):
+    return users.delete(id, response, db)
