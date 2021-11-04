ad0abb2feb63df0b970ce1070f83ee2e02c12892
alexercolin <alex_ercolin@hotmail.com>
2021-11-04 15:08:12 -0300

    refactor: Unencapsulates functons and routers of the main.py

--- blog/main.py
+++ blog/main.py
@@ -1,113 +1,11 @@
-from fastapi import FastAPI, Depends, status, Response, HTTPException
-from . import schemas, models
-from .database import engine, SessionLocal
-from sqlalchemy.orm import Session
-from pprint import pprint
-from typing import List
-from .hashing import Hash
-
+from fastapi import FastAPI
+from . import models
+from .database import engine
+from .routers import blog, users
 
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
 
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-# BLOGS
-
-
-@app.post('/blog', status_code=status.HTTP_201_CREATED)
-def create(request: schemas.Blog, db: Session = Depends(get_db)):
-    new_blog = models.Blog(title=request.title, body=request.body)
-    db.add(new_blog)
-    db.commit()
-    db.refresh(new_blog)
-    return new_blog
-
-
-@app.get('/blog', response_model=List[schemas.ShowBlog])
-def getAllBlogs(db: Session = Depends(get_db)):
-    blogs = db.query(models.Blog).all()
-    return blogs
-
-
-@app.get('/blog/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog)
-def getBlogById(id: int, response: Response, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
-    if not blog:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f'Blog with the id {id} is not available')
-
-    return blog
-
-
-@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def deleteBlogById(id: int, response: Response, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id)
-
-    if not blog.first():
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail='not found')
-
-    blog.delete(synchronize_session=False)
-    db.commit()
-    return'done'
-
-
-@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
-def updateBlogById(id, request: schemas.Blog, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id)
-
-    if not blog.first():
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail='not found')
-
-    blog.update({'title': request.title, 'body': request.body},
-                synchronize_session='evaluate')
-    db.commit()
-    return'done'
-
-# USERS
-
-
-@app.get('/user', response_model=List[schemas.Users])
-def getAllUsers(db: Session = Depends(get_db)):
-    users = db.query(models.User).all()
-    return users
-
-@app.get('/user/{id}',status_code=status.HTTP_200_OK, response_model=schemas.Users)
-def getUserById(id: int, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.id == id).first()
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f'User with the id {id} is not available')
-
-    return user
-
-@app.post('/user', status_code=status.HTTP_201_CREATED)
-def createUser(request: schemas.User, db: Session = Depends(get_db)):
-    new_user = models.User(
-        name=request.name, email=request.email, password=Hash.bcrypt(request.password))
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
-
-
-@app.delete('/user/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def deleteUser(id: int, response: Response, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.id == id)
-
-    if not user.first():
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail='not found')
-
-    user.delete(synchronize_session=False)
-    db.commit()
-    return'done'
+app.include_router(blog.router)
+app.include_router(users.router)
